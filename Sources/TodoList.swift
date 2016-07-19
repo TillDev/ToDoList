dbb3488532fda5bf7fa01290ecef1919525659bd
king <king@kings-MacBook-Pro.local>
2016-07-18 20:07:00 -0500

    all functions testing successfully

--- Sources/TodoList.swift
+++ Sources/TodoList.swift
@@ -22,11 +22,14 @@ import MySQL
 
 public class TodoList : TodoListAPI {
     
-    static let defaultHost = "127.0.0.1"
-    //static let defaultPort: UInt16
+    // TODO what does static keyword do here?
+    static let defaultHost = "127.0.0.1"  //TODO concat port
+    //static let defaultPort: UInt16 3306?
     static let defaultUser = "root"
     static let defaultPassword = ""
     static let defaultDatabase = "todolist"
+    
+    let lastInsertIdQuery = "SELECT LAST_INSERT_ID()"
     
     var mysql: MySQL.Database?
     var connection: Connection?
@@ -62,26 +65,76 @@ public class TodoList : TodoListAPI {
             connection = try mysql?.makeConnection()
         } catch {
             print("Error: \(error)")
+            //TODO what do to here?
         }
         
     }
     
+    //TODO hardcoded bool value for completed, since SELECT returns MySQL.Value.null
     
     
     public func count(withUserID: String?, oncompletion: (Int?, ErrorProtocol?) -> Void) {
-        // TODO:
+        
+        let user = withUserID ?? "default"
+        
+        do {
+            let query = "SELECT * FROM todos WHERE owner_id=\"\(user)\""
+            let results = try mysql?.execute(query)
+            
+            oncompletion(results?.count, nil)
+            
+        }
+        catch {
+            oncompletion(nil, TodoCollectionError.CreationError("There was a problem with the MySQL query"))
+        }
     }
     
     public func clear(withUserID: String?, oncompletion: (ErrorProtocol?) -> Void) {
-        // TODO:
+        
+        let user = withUserID ?? "default"
+        
+        do {
+            let query = "DELETE FROM todos WHERE owner_id=\"\(user)\""
+            try mysql?.execute(query)
+            
+            oncompletion(nil)
+            
+        }
+        catch {
+            oncompletion(TodoCollectionError.CreationError("Problem clearing table entries"))
+        }
     }
     
     public func clearAll(oncompletion: (ErrorProtocol?) -> Void) {
-        // TODO:
+        
+        do {
+            let query = "TRUNCATE TABLE todos"
+            
+            try mysql?.execute(query)
+            
+            oncompletion(nil)
+            
+        }
+        catch {
+            oncompletion(TodoCollectionError.CreationError("There was a problem clearing the table"))
+        }
     }
     
     public func get(withUserID: String?, oncompletion: ([TodoItem]?, ErrorProtocol?) -> Void) {
-        // TODO:
+        
+        let user = withUserID ?? "default"
+        
+        do {
+            let query = "SELECT * FROM todos WHERE owner_id=\"\(user)\""
+            let results = try mysql?.execute(query)
+            
+            let todos = try parseTodoItemList(results: results!)
+            oncompletion(todos, nil)
+            
+        }
+        catch {
+            oncompletion(nil, TodoCollectionError.CreationError("There was a problem retrieving TODO list items"))
+        }
     }
     
     public func get(withUserID: String?, withDocumentID: String, oncompletion: (TodoItem?, ErrorProtocol?) -> Void ) {
@@ -89,11 +142,29 @@ public class TodoList : TodoListAPI {
         let user = withUserID ?? "default"
         
         do {
-            let query = "SELECT * FROM todos WHERE owner_id=\"\(user)\" AND id=\"\(withDocumentID)\""
+            let query = "SELECT * FROM todos WHERE owner_id=\"\(user)\" AND tid=\"\(withDocumentID)\""
             let results = try mysql?.execute(query)
-            print("*** get results: \(results)")
             
-            //TODO construct todoitem to return
+            guard let order = results?[0]["orderno"]?.int else {
+                oncompletion(nil, TodoCollectionError.CreationError("Problem retrieving the TODO list item"))
+                return
+            }
+            
+            guard let title = results?[0]["title"]?.string else {
+                oncompletion(nil, TodoCollectionError.CreationError("Problem retrieving the TODO list item"))
+                return
+            }
+            
+            guard let completed = results?[0]["completed"]?.int else {
+                oncompletion(nil, TodoCollectionError.CreationError("Problem retrieving the TODO list item"))
+                return
+            }
+            
+            let completedValue = completed == 1 ? true : false
+            
+            let todoItem = TodoItem(documentID: withDocumentID, userID: user, order: order, title: title, completed: completedValue)
+            oncompletion(todoItem, nil)
+            
         }
         catch {
             print("Testing get item failed: \(error)")
@@ -106,37 +177,154 @@ public class TodoList : TodoListAPI {
         let user = userID ?? "default"
         
         do {
+            let completedValue = completed ? 1 : 0
             
-            let query = "INSERT INTO todos (title, owner_id, completed, orderno) VALUES ( \"\(title)\", \"\(user)\", \(completed), \(order))"
+            let query = "INSERT INTO todos (title, owner_id, completed, orderno) VALUES ( \"\(title)\", \"\(user)\", \(completedValue), \(order))"
             
             try mysql?.execute(query, [], connection)
             
-            let result = try mysql?.execute("SELECT LAST_INSERT_ID()", [], connection)
+            let result = try mysql?.execute(lastInsertIdQuery, [], connection)
             
-            if let documentID = (result?[0]["LAST_INSERT_ID()"])?.int {
-                
-                print("docID: \(documentID)")
-                let todoItem = TodoItem(documentID: String(documentID), userID: user, order: order, title: title, completed: completed)
-                
-                oncompletion(todoItem, nil)
-            }
-            else {
-                
+            guard result?.count == 1 else {
+                oncompletion(nil, TodoCollectionError.IDNotFound("There was a problem adding a TODO item"))
+                return
             }
             
+            guard let documentID = (result?[0]["LAST_INSERT_ID()"])?.int
+                where documentID > 0 else {
+                    oncompletion(nil, TodoCollectionError.IDNotFound("There was a problem adding a TODO item"))
+                    return
+            }
+            
+            let todoItem = TodoItem(documentID: String(documentID), userID: user, order: order, title: title, completed: completed)
+            oncompletion(todoItem, nil)
         }
+            
         catch {
-            print("Testing add item failed: \(error)")
+            oncompletion(nil, TodoCollectionError.CreationError("There was a problem adding a TODO item"))
+            return
         }
     }
     
     public func update(documentID: String, userID: String?, title: String?, order: Int?,
                        completed: Bool?, oncompletion: (TodoItem?, ErrorProtocol?) -> Void ) {
-        // TODO:
+        
+        let user = userID ?? "default"
+        
+        var originalTitle: String = "", originalOrder: Int = 0, originalCompleted: Bool = false
+        var titleQuery: String = "", orderQuery: String = "", completedQuery: String = ""
+        
+        if title == nil || order == nil || completed == nil {
+            
+            get(withUserID: userID, withDocumentID: documentID){
+                todo, error in
+                
+                if let todo = todo {
+                    originalTitle = todo.title
+                    originalOrder = todo.order
+                    originalCompleted = todo.completed
+                }
+            }
+        }
+        
+        let finalTitle = title ?? originalTitle
+        if (title != nil) {
+            titleQuery = " title=\"\(finalTitle)\","
+        }
+        
+        let finalOrder = order ?? originalOrder
+        if (order != nil) {
+            orderQuery = " orderno=\(finalOrder),"
+        }
+        
+        let finalCompleted = completed ?? originalCompleted
+        if (completed != nil) {
+            let completedValue = finalCompleted ? 1 : 0
+            completedQuery = " completed=\(completedValue),"
+        }
+        
+        var concatString = titleQuery + orderQuery + completedQuery
+        
+        let query = "UPDATE todos SET" + String(concatString.characters.dropLast()) + " WHERE tid=\"\(documentID)\""
+        
+        do {
+            
+            try mysql?.execute(query, [], connection)
+            
+            let todoItem = TodoItem(documentID: String(documentID), userID: user, order: finalOrder, title: finalTitle, completed: finalCompleted)
+            oncompletion(todoItem, nil)
+            
+        }
+        catch {
+            oncompletion(nil, TodoCollectionError.CreationError("There was a problem adding a TODO item"))
+            return
+        }
     }
     
     public func delete(withUserID: String?, withDocumentID: String, oncompletion: (ErrorProtocol?) -> Void) {
-        // TODO:
+        
+        let user = withUserID ?? "default"
+        
+        do {
+            
+            let query = "DELETE FROM todos WHERE owner_id=\"\(user)\" AND tid=\"\(withDocumentID)\""
+            
+            try mysql?.execute(query, [], connection)
+            
+            oncompletion(nil)
+        }
+            
+        catch {
+            oncompletion(TodoCollectionError.IDNotFound("There was a problem deleting the TODO item"))
+            return
+        }
+        
+    }
+    
+    private func parseTodoItemList(results: [[String : MySQL.Value]]) throws -> [TodoItem] {
+        
+        var todos = [TodoItem]()
+        for entry in results {
+            
+            let test: TodoItem = try createTodoItem(entry: entry)
+            todos.append(test)
+            
+        }
+        return todos
+    }
+    
+    private func createTodoItem(entry: [String : MySQL.Value]) throws -> TodoItem {
+        
+        var tid: Int = 0, user: String = "", title: String = "", orderno: Int = 0, completed: Int = 0
+        
+        for(key, value) in entry {
+            if key == "tid" {
+                tid = value.int!
+                continue
+            }
+            if key == "owner_id" {
+                user = value.string!
+                continue
+            }
+            if key == "title" {
+                title = value.string!
+                continue
+            }
+            if key == "orderno" {
+                orderno = value.int!
+                continue
+            }
+            if key == "completed" {
+                completed = 0 //value.int! //TODO what's up here?
+                continue
+            }
+        }
+        
+        let completedValue = completed == 1 ? true : false
+        
+        let todoItem = TodoItem(documentID: String(tid), userID: user, order: orderno, title: title, completed: completedValue)
+        return todoItem
+        
     }
     
 }
